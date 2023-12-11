80c96c0216c02aeec5135aa6436828509ce3307b
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2023-12-11 14:22:02 +0100

    Add new endpoint for translation

--- app/c.py
+++
@@ -1,7 +0,0 @@
-from optparse import OptionParser
-from config import Config
-
-cfg = Config('../config/prompts.ini')
-#cfg = config.Config(
-#cfg.addNamespace(options, 'cmdline')
-print("The verbose option value is %r" % cfg['sql_generator'])
