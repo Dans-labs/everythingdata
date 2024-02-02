6b04c49a4bca94286c137e4188826737088a8bdd
Vyacheslav Tykhonov <4tikhonov@gmail.com>
2024-02-02 20:37:46 +0100

    App updated with new endpoints

--- app/app.py
+++ app/app.py
@@ -1,14 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from nltk import tokenize, sent_tokenize
+from localconfig import SYS_PROMPT, R_SYS_PROMPT
+from langchain.llms import Ollama
 from fastapi.responses import HTMLResponse
+from nltk import tokenize, sent_tokenize
 import pandas as pd
+from utils import graphreader, generate_md5, infocleaner, getlanguage
 from config import Config
 import duckdb
 import time
 import os
 import json
-from llmframe import LLMFrame
+from llmframe import LLMFrame, keywords_cleaner
 from config import Config
 from typing import Optional
+from pydantic import BaseModel
 
 cfg = Config(os.environ['CONFIG'])
 token = os.getenv('TOKEN')
@@ -20,8 +26,22 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/translate")
-def translate(job='translate', customquery: Optional[str] = None, language: Optional[str] = None, format: Optional[str] = None):
+# Endpoint to handle POST requests
+@app.post("/graph/")
+async def graph_item(info: Request):
+    transferdata = await info.json()
+    llm = LLMFrame()
+    data = llm.graph_decompression(transferdata)
+    return data
+
+@app.get("/graph")
+def graph(job='custom', prompt='graph.prompt', inputtext: Optional[str] = None):
+    llm = LLMFrame()
+    data = llm.graph_decompression({'text': inputtext})
+    return data
+
+@app.get("/custom")
+def translate(job='custom', customquery: Optional[str] = None, language: Optional[str] = None, format: Optional[str] = None):
     query = None
     if not format:
         format = 'html'
@@ -30,6 +50,69 @@ def translate(job='translate', customquery: Optional[str] = None, language: Opti
     llm = LLMFrame(config=cfg[job], job=job)
     msg = llm.create_message(table_name = None, query = query, customquery=customquery)
     m = llm.create_message(table_name = None, query = query, customquery=customquery)
+    llm.debug_messages(m)
+    messages = llm.prepare_message(m)
+    o = llm.run_pipeline(messages)
+    if format == 'html':
+        return HTMLResponse(content=str(o[0]['generated_text']).replace('\n', '<br>'), status_code=200)
+    else:
+        return json.dumps(o)
+
+@app.get("/keywords")
+def translate(job='main_keywords', customquery: Optional[str] = None, language: Optional[str] = None, format: Optional[str] = None):
+    query = None
+    if not format:
+        format = 'html'
+    if not language:
+        language = 'English'
+    llm = LLMFrame(config=cfg[job], job=job)
+    msg = llm.create_message(table_name = None, query = query, customquery=customquery)
+    m = llm.create_message(table_name = None, query = query, customquery=customquery)
+    llm.debug_messages(m)
+    messages = llm.prepare_message(m)
+    o = keywords_cleaner(llm.run_pipeline(messages))
+    if format == 'html':
+        return HTMLResponse(content=str(o[0]['generated_text']).replace('\n', '<br>'), status_code=200)
+    else:
+        return json.dumps(o)
+
+@app.post("/translate/")
+async def translate_item(info: Request, job='translate'):
+    transferdata = await info.json()
+    if 'format' in transferdata:
+        format = transferdata['format']
+    else:
+        format = 'json'
+    llm = LLMFrame(config=cfg[job], job=job)
+    if 'concept' in transferdata:
+        m = llm.create_message(table_name = None, query = None, customquery=transferdata['concept'], context=transferdata['context'])
+        messages = llm.prepare_message(m)
+        o = llm.run_pipeline(messages)
+        if format == 'html':
+            return HTMLResponse(content=str(o[0]['generated_text']).replace('\n', '<br>'), status_code=200)
+        if format == 'json':
+            dataresponse = infocleaner(str(o[0]['generated_text']))
+            dataresponse['concept'] = transferdata['concept']
+            dataresponse['md5'] = generate_md5(dataresponse['concept']) 
+            dataresponse['md5context'] = generate_md5("%s %s" % (dataresponse['concept'], transferdata['context']))
+            dataresponse['lang'] = getlanguage(transferdata['context'])
+            return dataresponse
+        else:
+            return json.dumps(o)
+    return
+
+@app.get("/translate")
+def translate(job='translate', customquery: Optional[str] = None, language: Optional[str] = None, format: Optional[str] = None, context: Optional[str] = None):
+    query = None
+    if not format:
+        format = 'html'
+    if not language:
+        language = 'English'
+    if not context:
+        context = 'nocontext'
+    llm = LLMFrame(config=cfg[job], job=job)
+    msg = llm.create_message(table_name = None, query = query, customquery=customquery)
+    m = llm.create_message(table_name = None, query = query, customquery=customquery, context=context)
     llm.debug_messages(m)
     messages = llm.prepare_message(m)
     o = llm.run_pipeline(messages)
